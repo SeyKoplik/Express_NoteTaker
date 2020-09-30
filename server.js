d909cda8f0c9b0db78d1c778a62668edd699dee0
Seychelle Koplik <sey.koplik@gmail.com>
2020-09-30 14:32:22 -0400

    changed file routes

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+//add path
+var http = require("http");
+const fs = require("fs");
+​
+var app = express();
+var PORT = 5000;
+
+
+/// check starwars activities to get clues
+
+// API Routes
+
+// GET /api/notes
+// get data somehow from db.json
+//      return res.json(data); << at some point
+
+// POST /api/notes
+// receive JSON obj from frontend
+//      return rest.end(); << can also be used beside below
+//      return res.status(200).end(); << say everything worked correctly
+
+// DELETE /api/notes/:id
+    // /api/notes/1 << ref back to db.json
+
+// HTML Routes
+app.use(express.static("public"));
+
+// GET /notes >> goes to notes.html
+// GET * (everything) >> goes to index.html 
+//      * goes last because its everything after you should  get
+
+
+//URLs matter and key value pairs matter in index.js
+
