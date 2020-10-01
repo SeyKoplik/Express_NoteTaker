4c9c59df9b4eda79ddde180d27f209cc87d3a055
Seychelle Koplik <sey.koplik@gmail.com>
2020-10-01 09:57:25 -0400

    rearranged where to get dbjson correctly through fs.readfile now

--- server.js
+++ server.js
@@ -12,65 +12,81 @@ app.use(express.json());
 
 //============= Notes DATA
 // Obj Array need to go to db.json 
-const dbJSON = require('./db/db.json');
 
-console.log(dbJSON);
-
-// API Routes
-
-// GET /api/notes // get data from db.json
-app.get("/api/notes", function (req, res) {
-    return res.json(dbJSON); //<< data is db.json file data
-  });
-
-// POST /api/notes
-app.post("/api/notes", function (req, res) {
-    var newNote = req.body;
-
-    dbJSON.push(newNote);
-});
-// receive JSON obj from frontend
-//      return rest.end(); << can also be used beside below
-//      return res.status(200).end(); << say everything worked correctly
-
-// DELETE /api/notes/:id
-    // /api/notes/1 << ref back to db.json
-app.delete('/api/notes/:id', function (req, res) {
-    const id = req.params._id;
-
-    console.log(id);
-
-    //readfile db.json
-    //remove the note w/ the id specified
-    res.send('Got a DELETE request at /api/notes/:id');
-
-    for (var i = 0; i < notes.length; i++) {
-        if (id === notes[i].id) {
-          return res.json(notes[i]);
-        }
-      }
-    
-      return res.json(false);
-    });
-
-// HTML Routes
-// Code to serve images, CSS files, and JavaScript files in a directory named public
-app.use(express.static("public"));
-
-// GET /notes >> goes to notes.html
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
+fs.readFile('./db/db.json', (err, data) => {
+    if (err) throw err;
+    const dbJSON = JSON.parse(data);
+    console.log(dbJSON);
 });
 
-// GET * >> goes to index.html 
-// * goes last because its everything after you should get
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-});
+
+
+
+// // API Routes
+
+// // GET /api/notes // get data from db.json
+// app.get("/api/notes", function (req, res) {
+//     return res.json(dbJSON); //<< data is db.json file data
+// });
+
+// // POST /api/notes
+// app.post("/api/notes", function (req, res) {
+//     var newNote = req.body;
+//     // console.log(newNote); < returns what I want it to
+//     // readfile
+//     // give note new id of biggest id+1
+//     dbJSON.push(newNote);
+//     return res.status(200).end();
+// });
+// // receive JSON obj from frontend
+// // say everything worked correctly
+
+// // DELETE /api/notes/:id
+// // /api/notes/1 << ref back to db.json
+// app.delete('/api/notes/:id', function (req, res) {
+//     // for (var i = 0; i < dbJSON.length; i++) {
+//     // const id = req.indexOf(params[i]);
+//     // const id = req.findIndex(params);
+//     const id = req.params.id;
+//     console.log(id);
+//     // }
+
+//     //readfile db.json
+//     // fs.readFile(__dirname + "./db/db.json", 'utf8', function (err,  {
+//     //     data = dbJSON;
+//     //     delete data[data.indexOf(data.filter(function () {
+//     //         return fs.writeFile("db.json", data, function (err) {
+//     //             if (err) { return console.log(err) };
+//     //         })
+//     //     }))]
+//     // })
+//     // //remove the note w/ the id specified
+
+
+//     // var newDbJSON = dbJSON.filter(id);
+//     // console.log(newDbJSON);
+
+//     //   return res.json(newDbJSON);
+// });
+
+// // HTML Routes
+// // Code to serve images, CSS files, and JavaScript files in a directory named public
+// app.use(express.static("public"));
+
+// // GET /notes >> goes to notes.html
+// app.get("/notes", (req, res) => {
+//     res.sendFile(path.join(__dirname, "public/notes.html"));
+// });
+
+// // GET * >> goes to index.html 
+// // * goes last because its everything after you should get
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "public/index.html"));
+// });
 
 
 //URLs matter and key value pairs matter in index.js
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}/`)  
+    console.log(`Server is running on http://localhost:${PORT}/`)
 })
